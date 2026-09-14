67346a1bbfd8aaa13cd2da0d84d3ac93c346c8d3
Sofia1-min <sofiahortunova@gmail.com>
2026-09-14 19:59:34 +0300

    Add getter and static counter to Todo

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,14 +1,16 @@
 class Todo{
+  static int _counter = 0;
   int id;
   String title;
   bool isDone;
-Todo({required this.id, required this.title}) : isDone = false;
-@override 
-String toString(){
-  String status = isDone ? '[x]' : '[]';
-  return '$status $id.$title';}
+Todo({required this.title}) : id = ++_counter, isDone = false;
   void complete(){
     isDone = true;
   }
+String get status => isDone ? 'выполнено' : 'в процессе';
+@override 
+String toString(){
+  String mark = isDone ? '[x]' : '[]';
+  return '$mark $id.$title($status)';}
 
 }
